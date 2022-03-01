8e2aa6064963685d732c23bdb1b1402656e4471a
Diego Barbosa <diegobarbox@gmail.com>
2022-03-01 19:29:37 -0300

    Adicionando novas entradas.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('', views.index, name='index'),  # página inicial
     path('topics/', views.topics, name='topics'),
     path('<int:topic_id>/', views.topic, name='topic'),
+    path('new_topic/', views.new_topic, name='new_topic'),
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
 ]
